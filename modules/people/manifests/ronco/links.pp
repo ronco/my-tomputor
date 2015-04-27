62a75487a33653f2b5e51400c3be93882413401d
Ron <ronco@costite.com>
2015-04-27 14:32:42 -0600

    don't symlink .zshrc, rely on homeshick

--- modules/people/manifests/ronco/links.pp
+++ modules/people/manifests/ronco/links.pp
@@ -2,13 +2,6 @@ class people::ronco::links {
 
   $my_init_src = '/opt/init-src'
 
-  file { "/Users/${luser}/.zshrc":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_init_src}/dotfiles/home/.zshrc",
-    require => Repository["${my_init_src}/dotfiles"],
-  }
-
   file { "/Users/${luser}/.oh-my-zsh":
     ensure  => link,
     target  => "${my_init_src}/oh-my-zsh",
