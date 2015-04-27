374550bdb36ca5fef790a200cc73bef8288a93ac
Ron <ronco@costite.com>
2015-04-27 08:14:13 -0600

    switch to use brewcask for most apps

--- modules/people/manifests/ronco/applications.pp
+++ modules/people/manifests/ronco/applications.pp
@@ -1,19 +1,7 @@
 class people::ronco::applications {
-  include chrome
   include chrome::canary
-  include firefox
-  include slate
-
-  include virtualbox
-
-  class { 'vagrant':
-    version => '1.7.2'
-  }
 
   include iterm2::stable
   include iterm2::colors::solarized_dark
-
-  include onepassword
-  include onepassword::chrome
 
 }
